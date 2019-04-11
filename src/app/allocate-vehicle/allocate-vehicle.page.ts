eb775394428975c25ae423a036af582b84bb580d
Darryl Brown <anthony.brown.db@gmail.com>
2019-04-11 00:19:31 -0500

    printer 2

--- src/app/allocate-vehicle/allocate-vehicle.page.ts
+++ src/app/allocate-vehicle/allocate-vehicle.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { QRCService, qrcI } from '../services/qrcservice.service';
@@ -15,12 +15,15 @@ export class AllocateVehiclePage implements OnInit {
   License = '';
   userallocateid = '';
   qrcObj: qrcI;
+  encodedData:string="";
+  FPrinter: boolean=false;
 
   constructor(
     private router: Router,
     public platform: Platform,
     public afstore: AngularFirestore,
     private qrcService: QRCService,
+    private alertController: AlertController,
     private o_userService: OccupiedUserService
   ) {}
 
@@ -53,6 +56,17 @@ export class AllocateVehiclePage implements OnInit {
         userLicNbr: userLicense,
         userid: userallocateid
       });*/
+      if(this.FPrinter==true){
+        const alert = await this.alertController.create({
+          header: 'On Screen Ticket',
+          subHeader: 'SCIT Parking Lot',
+          message: "License Plate #:"+this.License+' \n\n\n '+this.encodedData,
+          translucent: true,
+          buttons: ['OK']
+        });
+        await alert.present();
+      }
+
   }
 
   back() {
